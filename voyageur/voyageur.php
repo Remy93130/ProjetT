57c1d8b398d0a5ce374dddedfa17ceeaa62e3c86
Remy93130 <32952895+Remy93130@users.noreply.github.com>
2018-01-16 15:45:29 +0100

    Voyageur
    A regler : prblm de responsivité

--- voyageur/voyageur.php
+++ voyageur/voyageur.php
@@ -20,26 +20,60 @@
 			<div class="list-group">
 				<h2 class="list-group-item active text-center">Stigmates de la seconde guerre mondiale</h2>
 				<div class="list-group-item">
-						<table>
-							<tr>
-								<th>
-									<img src="../images/voyageur1.jpg" class="table-col2">
-								</th>
-								<th>
-									<p class="text-justify table-col2">
-										Si Paris a été sauvé de la destruction, elle n'en a pas moins été bombardé et en porte encore les cicatrices à de très nombreux endroits dans la capitale.
-										En effet, au niveau de la rue d'Arcole, le haut mur de l'Hôtel Dieu porte encore la trace de deux tirs d'obus d'un char Allemand lors de la libération de Paris en Août 1944.	
-									</p>
-								</th>
-							</tr>
-						</table>
+					<table>
+						<tr>
+							<th>
+								<img src="../images/voyageur1.jpg" class="table-col2" alt="Image1">
+								<p class="text-center">Mur de l'Hôtel Dieu</p>
+							</th>
+							<th>
+								<p class="text-justify table-col2">
+									Si Paris a été sauvé de la destruction, elle n'en a pas moins été bombardé et en porte encore les cicatrices à de très nombreux endroits dans la capitale.
+									En effet, au niveau de la rue d'Arcole, le haut mur de l'Hôtel Dieu porte encore la trace de deux tirs d'obus d'un char Allemand lors de la libération de Paris en Août 1944.
+								</p>
+							</th>
+						</tr>
+					</table>
 				</div>
 			</div>
 			<br>
 			<div class="list-group text-center">
-				<h2 class="list-group-item active">Cat 2</h2>
-				<p class="list-group-item">Item</p>
-				<p class="list-group-item">Item</p>
+				<h2 class="list-group-item active">La fausse vieille maison médiévale</h2>
+				<div class="list-group-item">
+					<table>
+						<tr>
+							<th>
+								<img src="../images/voyageur2.jpg" class="table-col2" alt="Image2">
+								<p>La vieille maison médiévale par Fernand Pouillon 1958</p>
+							</th>
+							<th>
+								<p class="text-justify table-col2">
+									Sur l’Île de la Cité, à l’angle de la rue des Chantres et de la rue des Ursins, se trouve une maison qui n’a aucun équivalent dans la capitale. Une imposante porte en bois massif, de petites fenêtres à ogives, de jolies grilles en fer forgé, des pierres calcaires apparentes, une tour qui nous rappelle celles des fortifications médiévales… Cette construction semble nous donner un aperçu de ce à quoi ressemblaient les maisons de l’ancienne Lutèce, à l’époque où les premières pierres de la Cathédrale Notre-Dame ont été posées. Logique non, puisque nous sommes sur l’Île de la Cité, à deux pas du plus imposant édifice médiéval encore sur pied dans la capitale ?
+								</p>
+							</th>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<br>
+			<div class="list-group text-center">
+				<h2 class="list-group-item active">Le mystère de la statue d'Henri IV</h2>
+				<div class="list-group-item">
+					<table>
+						<tr>
+							<th>
+								<img src="../images/voyageur3.jpg" class="table-col2" alt="Image3">
+								<p class="text-center">La statue de Henri IV</p>
+							</th>
+							<th>
+								<p class="text-justify table-col2">
+									En effet, en 2004, à l’occasion de la restauration de la statue, sont mises à jours 7 boites scellées renfermant des documents des plus divers. Solennellement ouvertes par le ministre de la Culture, qui plus est devant une assemblée de journalistes intrigués, on y découvre 26 médailles retraçant les différentes étapes de la vie du roi, le récit du transport et un procès-verbal de l’inauguration de la statue, un exemplaire des Economies royales de Sully, un autre de la Henriade de Voltaire, et enfin une Histoire d’Henri le Grand de Péréfixe.
+									Pourtant la septième boite, cachée dans la tête du cavalier et sur le couvercle de laquelle est inscrit « Mesnel »,  ne livre pas immédiatement ses secrets: elle contient un parchemin enroulé et très détérioré. Ce n’est qu’à l’issue de minutieux travaux de restauration que l’on a découvert qu’il contenait la liste de tous les fondeurs et ciseleurs de la statue, sous la direction de Mesnel.
+								</p>
+							</th>
+						</tr>
+					</table>					
+				</div>
 			</div>
 		</div>
 	</div>
