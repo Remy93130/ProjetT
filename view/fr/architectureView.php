e3b706afda1f32279b62c4b1e9d85b93420d96b1
Remy <remy93130@gmail.com>
2018-05-07 10:26:42 +0200

    update architecture

--- view/fr/architectureView.php
+++ view/fr/architectureView.php
@@ -9,26 +9,27 @@ $pos = "architecture";
 			<h1 class="text-center">Architecture</h1>
 		</div>
 		<div class="row">
-			<p class="text-justify">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
+			<div class="col-md-8 col-md-offset-2">
+				<p class="text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
 		</div>
-		<hr>
 		<div class="row">
 			<h2 class="transit">Monuments</h2><br>
 			<div class="table-responsive">
-				<table class="table table-bordered table-responsive" style="font-size: 20px;">
+				<table class="table table-borderless table-responsive" style="font-size: 20px;">
 					<thead style="background-color: #B94503; color: #333333;">
 						<tr>
 							<th>Nom</th>
@@ -60,7 +61,6 @@ $pos = "architecture";
 				</table>
 			</div>
 		</div>
-		<hr>
 		<div class="row">
 			<h2 class="transit">Carte des monuments</h2><br>
 			<h4 class="text-center" style="padding: 25%;">Intégrer une map pour montrer les monuments</h4>
